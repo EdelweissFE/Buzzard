3bb0fafb0121b801c059b6e008923a6212f44812
Dummer Alexander <alexander.dummer@student.uibk.ac.at>
2021-02-03 17:06:04 +0100

    added missing files

---
+++ parameteridentifier.py
@@ -0,0 +1,23 @@
+import argparse
+
+from src.reader import inputReader
+from src.optimizer import runOptimization
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+            description='A tool for optimizing (material) parameters for multiple finite element simulations')
+    
+    parser.add_argument('file', type=str,  nargs = 1, )    
+    args=parser.parse_args()
+    
+    
+    inp = inputReader( args.file[0] )
+
+    print( inp )
+
+    optParams = runOptimization( inp )
+
+    print( optParams )
---
+++ src/reader.py
@@ -0,0 +1,63 @@
+from .simulation import Simulation
+from .material import Material
+
+
+def inputReader( filename ):
+    
+    keywords = ['*material', '*simulation', '*identify']
+
+    print( "reading input from", filename, "..." )
+    
+    inp = { keyword: [] for keyword in keywords }
+    
+    currKey = None
+    currInpString = ""
+
+    with open( filename, 'r') as f:
+
+        lines = f.readlines( )
+        
+        for line in lines:
+            
+            line = line.replace(" ", "")
+            if line == "\n":
+                continue
+            line = line.replace( "\n", ",")
+            split = line.split(',')
+             
+            if currKey is None:
+                if split[0] in keywords:
+                    currKey = split[0]
+                    currInpString = line 
+
+            else:
+                
+                if split[0] in keywords:
+                    # create object for old inputstring
+                    obj = createObjectForKeyword( currKey, currInpString )
+                    inp[currKey].append( obj )
+                    currKey = split[0]
+                    currInpString = line
+
+                else:
+                    currInpString += line
+
+        
+        # create object for old inputstring
+        obj = createObjectForKeyword( currKey, currInpString )
+        inp[currKey].append( obj )
+
+
+    return inp
+
+
+def createObjectForKeyword( keyword, inputstring ):
+
+    if keyword == "*simulation":
+        return Simulation( inputstring )
+    
+    elif keyword == "*material":
+        return Material( inputstring )
+    
+    else:
+        return inputstring
