0d0c6997f307f25abd24ddda354ba3b4d17ec6c0
Dummer Alexander <alexander.dummer@student.uibk.ac.at>
2022-07-27 11:02:57 +0200

    added license headers

--- buzzard.py
+++ buzzard.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+#  ---------------------------------------------------------------------
+#
+#  ____                             _
+# | __ ) _   _ __________ _ _ __ __| |
+# |  _ \| | | |_  /_  / _` | '__/ _` |
+# | |_) | |_| |/ / / / (_| | | | (_| |
+# |____/ \__,_/___/___\__,_|_|  \__,_|
+#
+#
+#  Unit of Strength of Materials and Structural Analysis
+#  University of Innsbruck,
+#  2021 - today
+#
+#  Alexander Dummer alexander.dummer@uibk.ac.at
+#
+#  This file is part of Buzzard.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  The full text of the license can be found in the file LICENSE.md at
+#  the top level directory of Buzzard.
+#  ---------------------------------------------------------------------
+
 import argparse
 import os
 
