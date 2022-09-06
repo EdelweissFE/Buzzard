bd7cccf287e5c4246abcb576bc1388a31a9c0a6e
alexdummer <alexdummer@users.noreply.github.com>
2022-09-06 12:17:10 +0000

    deploy: aea26cb47135648586fc63fac0b6b3fb82726609

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v22.08',
+    VERSION: 'v22.09',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
